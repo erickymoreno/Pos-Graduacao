8db64eeeedefa22111e41591af38dec7be838f62
erickymoreno <erickymorenoads@gmail.com>
2022-10-23 12:15:31 -0400

    Authentication with jwt

--- web-service/poss-api/src/app.module.ts
+++ web-service/poss-api/src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
+import { DatabaseModule } from './database/database.module';
+import { PostModule } from './post/post.module';
 import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [DatabaseModule, PostModule, AuthModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- web-service/poss-api/src/user/user.module.ts
+++ web-service/poss-api/src/user/user.module.ts
@@ -2,12 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from '../database/database.module';
 import { providers } from '../providers';
 import { UserService } from './user.service';
-import { TypeOrmModule } from '@nestjs/typeorm';
 import { UserController } from './user.controller';
-import { User } from './entities/user.entity';
 
 @Module({
-  imports: [DatabaseModule, TypeOrmModule.forFeature([User])],
+  imports: [DatabaseModule],
   controllers: [UserController],
   providers: [...providers, UserService],
   exports: [UserService],
